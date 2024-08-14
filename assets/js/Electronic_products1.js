ad62a01e6f0d7e8f0d4355237c37c943587a347c
CBadlani <chandanibadlani@gmail.com>
2024-08-14 18:35:19 +0530

    done

---
+++ assets/js/Electronic_products1.js
@@ -0,0 +1,88 @@
+const products = [
+    {
+        id: 1,
+        name: "Home Appliances",
+        price: 80.00,
+        image: "electronic_images/Household/img15.jpg",
+        description: "Durable wall-mounted fan with adjustable speed settings and oscillation for effective air circulation. Ideal for home and office use.."
+    },
+    {
+        id: 2,
+        name: "Home Appliances",
+        price: 10.00,
+        image: "electronic_images/Household/img16.jpeg",
+        description: "Energy-efficient LED electric bulb with a long lifespan and bright illumination. Suitable for various lighting needs at home or office.'"
+    },
+    {
+        id: 3,
+        name: "Home Appliances",
+        price: 15.00,
+        image: "electronic_images/Household/img17.jpeg",
+        description: "Smart LED bulb with adjustable brightness and color temperature. Compatible with smart home systems for remote control and scheduling."
+    },
+    {
+        id: 4,
+        name: "Home Appliances",
+        price: 120.00,
+        image: "electronic_images/Household/img18.jpeg",
+        description: "Compact microwave oven with multiple cooking modes, easy-to-use control panel, and a 20-liter capacity. Ideal for quick meal preparation and reheating."
+    },
+    {
+        id: 5,
+        name: "Home Appliances",
+        price: 650.00,
+        image: "electronic_images/Household/img19.jpeg",
+        description: "Energy-efficient front-load washing machine with a 7 kg capacity, multiple wash programs, and a high spin speed for effective cleaning and fast drying."
+    },
+    {
+        id: 6,
+        name: "Home Appliances",
+        price: 45.00,
+        image: "electronic_images/Household/img20.jpeg",
+        description: "Compact electric heater with adjustable temperature settings and rapid heating capabilities. Ideal for keeping small to medium-sized rooms warm and cozy."
+    },
+    {
+        id: 7,
+        name: "Home Appliances",
+        price: 25.00,
+        image: "electronic_images/fan5.jpeg",
+        description: "Compact table fan with adjustable speed settings and oscillation for efficient air circulation. Ideal for personal use on desks or tables."
+    },
+    {
+        id: 8,
+        name: "Home Appliances",
+        price: 150.00,
+        image: "electronic_images/Household/img21.jpeg",
+        description: "Energy-efficient evaporative air cooler with a large water tank, adjustable fan speeds, and a remote control for convenient operation. Ideal for cooling medium-sized rooms"
+    },
+    {
+        id: 9,
+        name: "Home Appliances",
+        price: 180.00,
+        image: "electronic_images/Household/img24.jpeg",
+        description: "Oil-filled radiator heater with multiple heat settings, adjustable thermostat, and a quiet operation. Provides consistent and efficient heating, ideal for cold climates."
+    },
+    {
+        id: 10,
+        name: "Home Appliances",
+        price: 25.00,
+        image: "electronic_images/Household/img23.jpeg",
+        description: "Versatile decorative LED string lights with multiple color options and lighting modes. Perfect for enhancing the ambiance of any space for events or everyday decor."
+    },
+    {
+        id: 11,
+        name: "Home Appliances",
+        price: 35.00,
+        image: "electronic_images/toy10.jpeg",
+        description: "Battery-operated electric race car with realistic sounds and lights. Features easy controls and durable design, perfect for kids who love fast-paced fun"
+    },
+    {
+        id: 14,
+        name: "Home Appliances",
+        price: 699.00,
+        image: "electronic_images/apple1.jpeg",
+        description: "Latest smartphone with a high-resolution display, powerful processor, and advanced camera system. Offers smooth performance and a sleek design.."
+    },
+];
+
+export default products;
