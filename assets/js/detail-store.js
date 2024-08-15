ec0d06054f53c04e5c5f95dd2a7998f716e4df89
suyashzendage <146823504+suyashzendage@users.noreply.github.com>
2024-08-15 22:27:41 +0530

    rimening

--- assets/js/detail-store.js
+++ assets/js/detail-store.js
@@ -1,4 +1,4 @@
-import products from '/products.js';
+import products from './products.js';
 import cart from './cart-store.js';
 
 
@@ -39,7 +39,7 @@ const initApp = () => {
         let newProduct = document.createElement('div');
         newProduct.classList.add('item');
         newProduct.innerHTML =
-            `<a href="/detail.html?id=${product.id}">
+            `<a href="/detail-store.html?id=${product.id}">
             <img src="${product.image}">
         </a>
         <h2>${product.name}</h2>
--- assets/js/index-store.js
+++ assets/js/index-store.js
@@ -19,24 +19,20 @@ const loadTemplate = () => {
 }
 loadTemplate();
 const initApp = () => {
-    // load list product
-    let listProductHTML = document.querySelector('.listProduct');
-    listProductHTML.innerHTML = null;
+        // load list product
+        let listProductHTML = document.querySelector('.listProduct');
+        listProductHTML.innerHTML = null;
 
-    products.forEach(product => {
-        let newProduct = document.createElement('div');
-        newProduct.classList.add('item');
-        newProduct.innerHTML =
-            `<a href="/detail.html?id=${product.id}">
+        products.forEach(product => {
+                    let newProduct = document.createElement('div');
+                    newProduct.classList.add('item');
+                    newProduct.innerHTML =
+                        `<a href="/detail-store.html?id=${product.id}">
              <img src="${product.image}">
          </a>
          <h2>${product.name}</h2>
          <div class="price">$${product.price}</div>
-         <button 
-             class="addCart" 
-             data-id='${product.id}'>
-                 Add To Cart
-         </button>`;
+        
         listProductHTML.appendChild(newProduct);
     });
 }
